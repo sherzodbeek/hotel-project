c2f3c2241af07969da205bfa0a09d992cb712f81
sherzodbeek <sherzodbeek98@gmail.com>
2021-06-15 14:55:05 +0500

    firstCommit

---
+++ src/test/java/uz/pdp/hotelproject/HotelProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.hotelproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
